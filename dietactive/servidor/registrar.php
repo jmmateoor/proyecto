79b232cdd8a6dc6bbd741d0c5d885761c6cebf32
Jose <josem.mateo.ortega@gmail.com>
2016-04-16 15:05:56 +0200

    Añadida la plantilla para la zona clientes

--- dietactive/servidor/registrar.php
+++ dietactive/servidor/registrar.php
@@ -39,7 +39,7 @@
 			$id=$idcliente;
 		}
 		
-		$path="http://localhost:8080/daw/proyecto/dietactive/servidor/activar_cuenta.php";
+		$path="http://localhost/daw/proyecto/dietactive/servidor/activar_cuenta.php";
 		$link=$path."?id=".$id."&codigo=".$codigo."";
 		
 		
@@ -49,12 +49,12 @@ Utiliza el siguiente enlace para activar tu cuenta
 		$asunto="Activación de cuenta en DietActive";
 		if(mail($email,$asunto,$mensaje))
 		{
-			echo "Se ha enviado un e-mail a tu correo electrónico para activar la cuenta. Verifica tu bandeja de entrada y tu carpeta de spam.";
+			echo "s";
 		}
 	}
 	else
 	{
-		echo "Ha ocurrido un error durante el registro. Intentalo de nuevo más tarde.";
+		echo "n";
 	}
 	$c->close();
 ?>
