c744d53a0a489e45daa55d8d6c7ecb32f4308983
Jose <josem.mateo.ortega@gmail.com>
2016-05-05 18:38:02 +0200

    Planteado el test de tres dias

---
+++ dietactive/servidor/consulta_alimentos.php
@@ -0,0 +1,23 @@
+<?php
+	include("config.inc.php");
+	include("funciones.inc.php");
+	$c = new MySQLi($servidor,$usuario,$password,$bbdd);
+	$c->set_charset("utf8");
+	$idtipoalimento=$_POST["idtipoalimento"];
+	$preparada = $c->prepare("select id, alimento from alimento where idtipoalimento = ? order by 2");
+	$preparada->bind_param("i",$idtipoalimento);
+	$preparada->execute();
+	$preparada->bind_result($id,$nombre);
+	$salida="[";
+	while($preparada->fetch())
+	{
+		$salida.="{";
+		$salida.="\"id\" : \"".$id."\",";
+		$salida.="\"alimento\" : \"".$nombre."\"";
+		$salida.="},";
+	}
+	$salida=substr($salida,0,-1);
+	$salida.="]";
+	echo $salida;
+	$c->close();
+?>
---
+++ dietactive/servidor/consulta_tipos_alimentos.php
@@ -0,0 +1,21 @@
+<?php
+	include("config.inc.php");
+	include("funciones.inc.php");
+	$c = new MySQLi($servidor,$usuario,$password,$bbdd);
+	$c->set_charset("utf8");
+	$preparada = $c->prepare("select id, nombre from tipoalimento order by 2");
+	$preparada->execute();
+	$preparada->bind_result($id,$nombre);
+	$salida="[";
+	while($preparada->fetch())
+	{
+		$salida.="{";
+		$salida.="\"id\" : \"".$id."\",";
+		$salida.="\"nombre\" : \"".$nombre."\"";
+		$salida.="},";
+	}
+	$salida=substr($salida,0,-1);
+	$salida.="]";
+	echo $salida;
+	$c->close();
+?>
